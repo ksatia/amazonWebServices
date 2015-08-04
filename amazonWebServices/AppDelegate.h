2d372617b36b52ac8c86ac87695e833201ae07d1
Karan Satia <your.ksatia@gmail.com>
2015-08-04 15:54:27 -0400

    Initial Commit

---
+++ amazonWebServices/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  amazonWebServices
+//
+//  Created by Karan Satia on 8/4/15.
+//  Copyright (c) 2015 Karan Satia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
